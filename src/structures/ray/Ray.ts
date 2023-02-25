734737f2997a829083a13124fecb2cf4040ca449
Eugene Pashkovskiy <pashkovskiy.eugen@gmail.com>
2023-02-25 21:38:08 +0200

    Added classed for vector, vertex and ray

---
+++ src/structures/ray/Ray.ts
@@ -0,0 +1,23 @@
+import Vector3D from '../vector/Vector3D';
+import Vertex3D from '../vertex/Vertex3D';
+
+export default class Ray {
+  public readonly position: Vertex3D;
+  public readonly vector: Vector3D;
+
+  constructor(position: Vertex3D, vector: Vector3D) {
+    this.position = position;
+    this.vector = vector;
+  }
+
+  public hasInside(vertex: Vertex3D): boolean {
+    // position + vector * t = vertex => solve for t; t = ((vertex - position) * vector) / (vector * vector)
+    const t =
+      vertex
+        .toVector()
+        .subtract(this.position.toVector())
+        .dotProduct(this.vector) / this.vector.dotProduct(this.vector);
+
+    return t >= 0;
+  }
+}
