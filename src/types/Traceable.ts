86c4f8ad4b58b1ac42779676bdc4ca3a8fefa6dd
liubazamula <liubazamula@liubazamula.liubazamula>
2023-03-04 08:01:27 +0100

    fix mistakes

--- src/types/Traceable.ts
+++ src/types/Traceable.ts
@@ -1,5 +1,5 @@
-import Ray from "../structures/ray/Ray";
+import Ray from '../structures/ray/Ray';
 
 export interface Traceable {
-    isIntersecting: (ray: Ray) => boolean
+  isIntersecting: (ray: Ray) => boolean;
 }
