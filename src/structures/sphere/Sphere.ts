d17e3deced7333bdc0d63b82135a136495078a44
liubazamula <liubazamula@liubazamula.liubazamula>
2023-03-04 08:29:07 +0100

    Added tests for sphere

--- src/structures/sphere/Sphere.ts
+++ src/structures/sphere/Sphere.ts
@@ -15,10 +15,21 @@ export class Sphere implements Traceable {
     const a = ray.vector.dotProduct(ray.vector);
     // o - c vector
     const ocVector = ray.position.toVector().subtract(this.center.toVector());
-    const b2 =
-      4 * ray.vector.dotProduct(ray.vector) * ocVector.dotProduct(ocVector);
+    //const b2 =
+      //4 * ray.vector.dotProduct(ray.vector) * ocVector.dotProduct(ocVector);
+    const b = 2 * ray.vector.dotProduct(ocVector);
     const c = ocVector.dotProduct(ocVector) - this.radius ** 2;
-    const D = b2 - 4 * a * c;
-    return D >= 0;
+    const D = b * b - 4 * a * c
+    //const D = b2 - 4 * a * c;
+    if (D < 0) {
+      return false;
+    } else if (D === 0) {
+      const t = -b / (2 * a);
+      return t >= 0;
+    } else {
+      const t1 = (-b + Math.sqrt(D)) / (2 * a);
+      const t2 = (-b - Math.sqrt(D)) / (2 * a);
+      return t1 >= 0 || t2 >= 0;
+    }
   }
 }
