4f374ddd45ba0010e1cd6820b5f6f4985b1a72a3
liubazamula <liubazamula@liubazamula.liubazamula>
2023-03-06 23:10:03 +0100

    Added method to recieve angle of intersection Ray and Plane

--- src/structures/plane/Plane.ts
+++ src/structures/plane/Plane.ts
@@ -1,3 +1,4 @@
+import Ray from '../ray/Ray';
 import Vector3D from '../vector/Vector3D';
 import Vertex3D from '../vertex/Vertex3D';
 
@@ -8,5 +9,23 @@ export default class Plane {
   constructor(vector: Vector3D, point: Vertex3D) {
     this.vector = vector;
     this.point = point;
+  }
+  
+  public intersection(ray: Ray): number | null {
+    const denominator = this.vector.dotProduct(ray.vector);
+    //accos a
+    //cos α = (n · d) / (|n| |d|),
+
+    const cosAngle = denominator / (this.vector.length * ray.vector.length);
+    if (denominator === 0) {
+      //if the ray is paralel to the plane
+      return null;
+    } else if ( cosAngle <=0 ){
+      //the intersection is behind the ray origin
+      return null;
+    } else {
+      const angle = Math.acos(cosAngle);
+      return angle;
+    }
   }
 }
