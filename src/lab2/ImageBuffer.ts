5fe3c9482cb2142927a247d667ad827d76eff720
liubazamula <liubazamula@liubazamula.liubazamula>
2023-04-02 19:00:01 +0200

    Write for ppm file

--- src/lab2/ImageBuffer.ts
+++ src/lab2/ImageBuffer.ts
@@ -1,11 +1,12 @@
 import { ImageInfo } from './interfaces/ImageInfo';
 import { Pixel } from './interfaces/Pixel';
+import { Readable } from 'stream';
 
 export class ImageBuffer {
   public readonly imageInfo: ImageInfo;
-  public readonly pixels: ReadableStream<Pixel>;
+  public readonly pixels: Readable;
 
-  constructor(imageInfo: ImageInfo, pixels: ReadableStream<Pixel>) {
+  constructor(imageInfo: ImageInfo, pixels: Readable) {
     this.imageInfo = imageInfo;
     this.pixels = pixels;
   }
