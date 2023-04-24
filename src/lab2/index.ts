e9e139fd8a5c76d01ef358520333c2c6c93a1e9a
Sviar <svpetrenko123@gmail.com>
2023-04-24 11:32:47 +0300

    update ppm

--- src/lab2/index.ts
+++ src/lab2/index.ts
@@ -9,23 +9,23 @@ import { argv } from 'process';
 import { PassThrough } from 'stream';
 import { ImageBuffer } from './ImageBuffer';
 
-const pixels = new PassThrough({ objectMode: true });
-pixels.push({ r: 0, g: 0, b: 0 });
-pixels.push({ r: 255, g: 255, b: 0 });
-pixels.push({ r: 255, g: 255, b: 255 });
-pixels.push({ r: 0, g: 0, b: 0 });
-pixels.push(null);
-const imageBuffer = new ImageBuffer(
-  {
-    height: 2,
-    width: 2,
-    maxColor: 255,
-  },
-  pixels
-);
-const stream = new WriterPNG().write(imageBuffer);
-const fileStream = createWriteStream(path.resolve(__dirname, 'file.png'));
-stream.pipe(fileStream);
+// const pixels = new PassThrough({ objectMode: true });
+// pixels.push({ r: 0, g: 0, b: 0 });
+// pixels.push({ r: 255, g: 255, b: 0 });
+// pixels.push({ r: 255, g: 255, b: 255 });
+// pixels.push({ r: 0, g: 0, b: 0 });
+// pixels.push(null);
+// const imageBuffer = new ImageBuffer(
+//   {
+//     height: 2,
+//     width: 2,
+//     maxColor: 255,
+//   },
+//   pixels
+// );
+// const stream = new WriterPNG().write(imageBuffer);
+// const fileStream = createWriteStream(path.resolve(__dirname, 'file.png'));
+// stream.pipe(fileStream);
 
 // const readStream = createReadStream(
 //   path.resolve(__dirname, 'plugins/png/samples/cubes.png')
@@ -43,12 +43,12 @@ stream.pipe(fileStream);
 //   writerPng.write(imageBuffer).pipe(writeStream);
 // });
 
-// const main = async () => {
-//   const loadingPath = argv[3];
-//   const imageProcessorLoader = new ImageProcessorLoader();
-//   const processors = await imageProcessorLoader.dynamicallyLoad(loadingPath);
-//   const imageMap = new ImageProcessorsMap();
-//   console.log(processors);
-//   imageMap.fillMaps(processors.readers, processors.writers);
-// };
-// main();
+const main = async () => {
+  const loadingPath = argv[3];
+  const imageProcessorLoader = new ImageProcessorLoader();
+  const processors = await imageProcessorLoader.dynamicallyLoad(loadingPath);
+  const imageMap = new ImageProcessorsMap();
+  console.log(processors);
+  imageMap.fillMaps(processors.readers, processors.writers);
+};
+main();
