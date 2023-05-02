982cecb43c75581c990e4c8d12f41a38fd6165a7
SJzamula <zamula.l08l@gmail.com>
2023-05-02 22:04:16 +0200

    Tests for Reader PPM

--- src/lab2/index.ts
+++ src/lab2/index.ts
@@ -5,22 +5,22 @@ import { createReadStream, createWriteStream } from 'fs';
 import path from 'path';
 import { ReaderPPM } from './plugins/ppm/ReaderPPM';
 
-// const pixels = new PassThrough({ objectMode: true });
-// pixels.push({ r: 0, g: 0, b: 0 });
-// pixels.push({ r: 255, g: 255, b: 255 });
-// pixels.push({ r: 255, g: 255, b: 255 });
-// pixels.push({ r: 0, g: 0, b: 0 });
-// const imageBuffer = new ImageBuffer(
-//   {
-//     height: 2,
-//     width: 2,
-//     maxColor: 255,
-//   },
-//   pixels
-// );
-// const stream = new WriterPPM().write(imageBuffer);
-// const fileStream = createWriteStream(path.resolve(__dirname, 'file.ppm'));
-// stream.pipe(fileStream);
+const pixels = new PassThrough({ objectMode: true });
+pixels.push({ r: 0, g: 0, b: 0 });
+pixels.push({ r: 255, g: 255, b: 255 });
+pixels.push({ r: 255, g: 255, b: 255 });
+pixels.push({ r: 0, g: 0, b: 0 });
+const imageBuffer = new ImageBuffer(
+  {
+    height: 2,
+    width: 2,
+    maxColor: 255,
+  },
+  pixels
+);
+const stream = new WriterPPM().write(imageBuffer);
+const fileStream = createWriteStream(path.resolve(__dirname, 'file.ppm'));
+stream.pipe(fileStream);
 
 const reader = new ReaderPPM();
 const inputStream = createReadStream('/home/liubazamula/projects/image.ppm');
@@ -30,8 +30,6 @@ reader
   .then((imageBuffer) => {
     if (imageBuffer) {
       console.log(imageBuffer);
-      const stream = new WriterPPM().write(imageBuffer);
-      const fileStream = createWriteStream(path.resolve(__dirname, 'file.ppm'));
       stream.pipe(fileStream);
     } else {
       console.error('Unable to read image file');
