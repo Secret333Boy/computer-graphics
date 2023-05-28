df82664e6dc5e8f5f75faa82d1d77138005dbd26
Sviar <svpetrenko123@gmail.com>
2023-05-28 08:22:18 +0300

    boilerplate

--- src/lab4/structures/KDTree.ts
+++ src/lab4/structures/KDTree.ts
@@ -1,5 +1,9 @@
+import Ray from '../../lab1/structures/ray/Ray';
+import { Hit } from '../../lab1/types/Hit';
 import { Traceable } from '../../lab1/types/Traceable';
 
 export class KDTree implements Traceable {
-  
+  getIntersection(ray: Ray): Hit | null {
+    throw new Error('Method not implemented.');
+  }
 }
